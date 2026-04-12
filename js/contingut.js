85a30335e98838e4c746a5506e8e4f69e7382a3f
xavicifpfbmoll <xjanercapataz@cifpfbmoll.eu>
2026-04-12 11:31:00 +0200

    Peuqeños retoques JS

--- js/contingut.js
+++ js/contingut.js
@@ -1,6 +1,5 @@
 function cambiarTema() {
     const boton = document.getElementById('botonTema');
-    
     if (document.body.classList.contains('modo-oscuro')) {
         document.body.classList.remove('modo-oscuro');
         boton.textContent = 'Modo Oscuro';
@@ -10,16 +9,15 @@ function cambiarTema() {
     }
 }
 
-// Efectos simples de ratón
+// efectos simples en botonTema
 const boton = document.getElementById('botonTema');
 boton.onmouseover = function() { this.style.transform = 'scale(1.15)'; this.style.boxShadow = '0 0 10px rgba(255, 193, 7, 0.6)'; };
 boton.onmouseout = function() { this.style.transform = 'scale(1)'; this.style.boxShadow = 'none'; };
 
-// Mostrar/Ocultar Formulario
+// boton formulario
 function mostrarOcultar() {
     const formulario = document.getElementById('formularioValoracion');
     const botonForm = document.getElementById('botonFormulario');
-    
     if (formulario.style.display === 'none') {
         formulario.style.display = 'flex';
         botonForm.textContent = 'Ocultar Formulario';
